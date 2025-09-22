16d8d73a0a5c602d7788e9c95a561a8871458489
Maurice <weber.maurice@proton.me>
2025-09-22 22:09:48 +0200

    add: updated transfer function

--- simple_transfer/transfer_app/src/main.rs
+++ simple_transfer/transfer_app/src/main.rs
@@ -7,6 +7,7 @@ use arm::{
     evm::CallType,
     merkle_path::MerklePath,
     nullifier_key::{NullifierKey, NullifierKeyCommitment},
+    resource::Resource,
     transaction::Transaction,
     utils::{bytes_to_words, words_to_bytes},
 };
@@ -48,7 +49,7 @@ pub fn default_values() -> SetUp {
             .parse()
             .expect("should parse private key"),
         erc20: address!("0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"), // USDC
-        amount: U256::from(1),
+        amount: U256::from(10),
         nonce: U256::from(1),
         deadline: U256::from(1893456000),
         spender: address!("0xc797A7708655b0e0907DABCb2246e07De91F7AF8"), // deployed for new logic-ref
@@ -92,7 +93,7 @@ fn example_keychain() -> KeyChain {
 fn simple_mint_test(
     data: &SetUp,
     keychain: &KeyChain
-) -> Transaction {
+) -> (Transaction, Resource) {
     let consumed_resource = construct_ephemeral_resource(
         &data.spender.to_vec(),
         &data.erc20.to_vec(),
@@ -146,7 +147,7 @@ fn simple_mint_test(
         data.nonce.to_be_bytes_vec(),
         data.deadline.to_be_bytes_vec(),
         permit_sig.as_bytes().to_vec(),
-        created_resource,
+        created_resource.clone(),
         keychain.discovery_pk,
         keychain.encryption_pk
     );
@@ -157,91 +158,47 @@ fn simple_mint_test(
     } else {
         println!("Transaction not verified");
     }
-    tx
+    (tx, created_resource)
 }
 
-fn create_test_transfer() -> Transaction {
-    let forwarder_addr = vec![1u8; 20];
-    let token_addr = vec![2u8; 20];
-    let quantity = 100;
-    // Obtain the consumed resource data
-    let consumed_auth_sk = AuthorizationSigningKey::new();
-    let consumed_auth_pk = AuthorizationVerifyingKey::from_signing_key(&consumed_auth_sk);
-    let (consumed_nf_key, consumed_nf_cm) = NullifierKey::random_pair();
-    let (consumed_discovery_sk, consumed_discovery_pk) = random_keypair();
-    let (consumed_encryption_sk, consumed_encryption_pk) = random_keypair();
-    let consumed_resource = construct_persistent_resource(
-        &forwarder_addr, // forwarder_addr
-        &token_addr,     // token_addr
-        quantity,
-        vec![4u8; 32], // nonce
-        consumed_nf_cm,
-        vec![5u8; 32], // rand_seed
-        &consumed_auth_pk,
-    );
-    let consumed_nf = consumed_resource.nullifier(&consumed_nf_key).unwrap();
-
+fn create_test_transfer(
+    data: &SetUp,
+    keychain: &KeyChain,
+    resource_to_transfer: &Resource,
+) -> Transaction {
+    let consumed_nf = resource_to_transfer.nullifier(&keychain.nf_key).unwrap();
+    
     // Create the created resource data
-    let created_auth_sk = AuthorizationSigningKey::new();
-    let created_auth_pk = AuthorizationVerifyingKey::from_signing_key(&created_auth_sk);
-    let (_created_nf_key, created_nf_cm) = NullifierKey::random_pair();
-    let (_created_discovery_sk, created_discovery_pk) = random_keypair();
-    let (_created_encryption_sk, created_encryption_pk) = random_keypair();
     let created_resource = construct_persistent_resource(
-        &forwarder_addr, // forwarder_addr
-        &token_addr,     // token_addr
-        quantity,
+        &data.spender.to_vec(), // forwarder_addr
+        &data.erc20.to_vec(),     // token_addr
+        data.amount.try_into().unwrap(),
         consumed_nf.as_bytes().to_vec(), // nonce
-        created_nf_cm,
+        keychain.nullifier_key_commitment(),
         vec![7u8; 32], // rand_seed
-        &created_auth_pk,
+        &keychain.auth_verifying_key(),
     );
     let created_cm = created_resource.commitment();
 
     // Get the authorization signature, it can be from external signing(e.g. wallet)
     let action_tree = MerkleTree::new(vec![consumed_nf, created_cm]);
-    let auth_sig = authorize_the_action(&consumed_auth_sk, &action_tree);
+    let auth_sig = authorize_the_action(&keychain.auth_signing_key, &action_tree);
 
     // Construct the transfer transaction
     let is_left = false;
     let path: &[(Vec<u32>, bool)] = &[(bytes_to_words(empty_leaf_hash().as_slice()), is_left)];
     let merkle_path = MerklePath::from_path(path);
 
-    let tx_start_timer = std::time::Instant::now();
     let tx = transfer::construct_transfer_tx(
-        consumed_resource.clone(),
+        resource_to_transfer.clone(),
         merkle_path.clone(),
-        consumed_nf_key.clone(),
-        consumed_auth_pk,
+        keychain.nf_key.clone(),
+        keychain.auth_verifying_key(),
         auth_sig,
         created_resource.clone(),
-        created_discovery_pk,
-        created_encryption_pk,
+        keychain.discovery_pk,
+        keychain.encryption_pk,
     );
-    println!("Tx build duration time: {:?}", tx_start_timer.elapsed());
-
-    // check the discovery ciphertexts
-    let discovery_ciphertext = Ciphertext::from_words(
-        &tx.actions[0].logic_verifier_inputs[1]
-            .app_data
-            .discovery_payload[0]
-            .blob,
-    );
-    discovery_ciphertext
-        .decrypt(&_created_discovery_sk)
-        .unwrap();
-
-    // check the encryption ciphertexts
-    let encryption_ciphertext = Ciphertext::from_words(
-        &tx.actions[0].logic_verifier_inputs[1]
-            .app_data
-            .resource_payload[0]
-            .blob,
-    );
-    let decrypted_resource = encryption_ciphertext
-        .decrypt(&_created_encryption_sk)
-        .unwrap();
-    assert_eq!(decrypted_resource, created_resource.to_bytes());
 
     // Verify the transaction
     if tx.clone().verify() {
@@ -253,7 +210,6 @@ fn create_test_transfer() -> Transaction {
 }
 
 pub fn submit_transaction(transaction: Transaction) {
-    // let rt = Builder::new_current_thread().enable_all().build().unwrap();
     let rt = runtime::Runtime::new().unwrap();
 
     let _ = rt.block_on(async { submit(transaction).await });
@@ -264,13 +220,14 @@ fn main() {
     let data: SetUp = default_values();
     let keychain: KeyChain = example_keychain();
 
-    let mint_tx = simple_mint_test(&data, &keychain);
+    let (mint_tx, minted_resource) = simple_mint_test(&data, &keychain);
     println!("Mint tx: {:?}", mint_tx);
+    println!("Minted resource: {:?}", minted_resource);
     let _ = submit_transaction(mint_tx);
 
-    // let transfer_tx = create_test_transfer();
-    // println!("Transfer tx: {:?}", transfer_tx);
+    let transfer_tx = create_test_transfer(&data, &keychain, &minted_resource);
+    println!("Transfer tx: {:?}", transfer_tx);
+    let _ = submit_transaction(transfer_tx);
 
-    // let _ = submit_transaction(tx);
     println!("Yippie");
 }
